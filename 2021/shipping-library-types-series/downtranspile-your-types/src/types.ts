b36df676401a24ec8754722f3d2b81e5a214da89
Martin Hochel <hochelmartin@gmail.com>
2022-03-23 22:26:26 +0100

    fixup! feat(downtranspile-types): simplify demo

--- 2021/shipping-library-types-series/downtranspile-your-types/src/types.ts
+++ 2021/shipping-library-types-series/downtranspile-your-types/src/types.ts
@@ -36,6 +36,9 @@ export function tail<T extends any[]>(arr: readonly [any, ...T]) {
 }
 
 // TS 4.1 - Template Literal Types
+type World = 'world'
+
+export type Greeting = `hello ${World}`
 type Color = 'red' | 'blue'
 type Quantity = 'one' | 'two'
 export type SeussFish = `${Quantity | Color} fish`
@@ -45,9 +48,25 @@ export interface Misc {
   greeting: `hello ${World}`
 }
 
-type World = 'world'
+// TS 4.4 Symbol and Template String Pattern Index Signatures
+interface Options {
+  width?: number
+  height?: number
+}
 
-export type Greeting = `hello ${World}`
+interface OptionsWithDataProps extends Options {
+  // Permit any property starting with 'data-'.
+  [optName: `data-${string}`]: unknown
+}
+let b: OptionsWithDataProps = {
+  width: 100,
+  height: 100,
+  'data-blah': true,
+
+  // Fails for a property which is not known, nor
+  // starts with 'data-'
+  // "unknown-property": true,
+}
 
 // TS 4.1 - Key Remapping in Mapped Types
 type RemoveKindField<T> = {
@@ -63,3 +82,16 @@ export type KindlessCircle = RemoveKindField<Circle>
 
 // TS 4.2 - spread in tuples anywhere
 export type TupleWithSpreadInside = [boolean, ...string[], boolean]
+
+// TS 4.5  Awaited helper
+export type Aawaited = Awaited<Promise<string>>
+export type Bawaited = Awaited<Promise<Promise<number>>>
+export type Cawaited = Awaited<boolean | Promise<number>>
+
+type NativeAwaited<T> = T extends null | undefined
+  ? T // special case for `null | undefined` when not in `--strictNullChecks` mode
+  : T extends object & { then(onfulfilled: infer F): any } // `await` only unwraps object types with a callable `then`. Non-object types are not unwrapped
+  ? F extends (value: infer V, ...args: any) => any // if the argument to `then` is callable, extracts the first argument
+    ? NativeAwaited<V> // recursively unwrap the value
+    : never // the argument to `then` was not callable
+  : T // non-object or non-thenable
